729ce7d95370206ffad4614e5b338020eb4a6516
Soy Zen <noobiezz2020@gmail.com>
2024-06-26 20:06:40 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// function handleLoginClick() {
+//   // Hide both login and signup buttons
+//   document.getElementById("login").style.display = "none";
+//   document.getElementById("signup").style.display = "none";
+
+//   // Show the user profile
+//   document.getElementById("border-user").style.display = "block";
+// }
+
+// document.getElementById("login").addEventListener("click", handleLoginClick);
+// localStorage.setItem('buttonText', newText);
+
+
